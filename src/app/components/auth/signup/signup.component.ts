9610c7c10b517b7165ee9321650a605c3757e0c8
fferreira12 <fferreira12@gmail.com>
2019-05-18 11:33:56 -0300

    Improves signin and signup

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 
-import { AuthService } from 'src/app/services/auth.service';
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor(private authService: AuthService, private router: Router) { }
+  error = null;
+  loading: boolean = false;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSignup(form: NgForm) {
+    this.loading = true;
     const email = form.value.email;
     const password = form.value.password;
-    this.authService.signupUser(email, password)
-      .then(data => 
-        this.router.navigate(['/signin'])
-      );
-  }
 
+    this.authService
+      .signupUser(email, password)
+      .then(data => {
+        this.loading = false;
+        if (data) {
+          this.router.navigate(["/signin"]);
+        } else {
+          this.error = {
+            message: "Could not create account"
+          };
+        }
+      });
+  }
 }
