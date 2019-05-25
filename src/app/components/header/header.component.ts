b76182c263c3e5eb4533bd801f8c0f2d16db4ac0
fferreira12 <fferreira12@gmail.com>
2019-05-25 11:07:01 -0300

    Displays user email at header when logged in

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
-import { SplitterService } from 'src/app/services/splitter.service';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { SplitterService } from "src/app/services/splitter.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  currentLoggedInAs: string;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.authService.subscribeToUser(user => {
+      if (user == null) {
+        return;
+      }
+      this.currentLoggedInAs = user.email;
+    });
   }
-
 }
