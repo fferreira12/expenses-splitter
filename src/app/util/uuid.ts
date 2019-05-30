b4f8c22ac077526d884b5327180880cb72863f0f
Fernando Ferreira <fferreira12@gmail.com>
2019-05-30 13:16:35 -0300

    Adds the ability to add one suggested payment to the actual payments

--- src/app/util/uuid.ts
+++ src/app/util/uuid.ts
@@ -3,7 +3,7 @@ export function uuid() {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
-  console.log('id');
-  console.log(id);
+  //console.log('id');
+  //console.log(id);
   return id;
 }
