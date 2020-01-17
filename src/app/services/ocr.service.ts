c3b9ca062bc91589eb0a4957f645092a2a835e36
fferreira12 <fferreira12@gmail.com>
2020-01-17 18:38:08 -0300

    Prepares OCR worker in the background

--- src/app/services/ocr.service.ts
+++ src/app/services/ocr.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import * as Teseract from "tesseract.js";
+import { createWorker } from 'tesseract.js';
 import { from, Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -8,14 +9,28 @@ import { map } from "rxjs/operators";
 })
 export class OcrService {
   public result$: Observable<Teseract.RecognizeResult>;
+  private worker: Teseract.Worker;
+  private workerReady = false;
 
-  constructor() {}
-
-
-  recognizeText(image: Teseract.ImageLike) {
-    let promise = Teseract.recognize(image, "por", {
+  constructor() {
+    this.worker = createWorker({
       logger: m => console.log(m)
     });
+    this.prepareWorker();
+  }
+
+  async prepareWorker() {
+    await this.worker.load();
+    await this.worker.loadLanguage('por');
+    await this.worker.initialize('por');
+    this.workerReady = true;
+  }
+
+  recognizeText(image: Teseract.ImageLike) {
+    if(!this.workerReady) {
+      return;
+    }
+    let promise = this.worker.recognize(image);
     this.result$ = from(promise);
     return this.result$.pipe(map(result => result.data.text));
   }
