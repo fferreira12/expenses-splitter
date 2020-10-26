b1a532a9ee0a6f49739df38a2127d8b43d660b94
Fernando Ferreira <fferreira12@gmail.com>
2020-10-25 22:27:53 -0300

    Add summary cards to user report

--- src/app/components/report/user-report/user-report.component.ts
+++ src/app/components/report/user-report/user-report.component.ts
@@ -3,8 +3,10 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { combineLatest, Observable } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
+import { Expense } from 'src/app/models/expense.model';
 import { Project } from 'src/app/models/project.model';
 import { ExpenseTableData } from 'src/app/models/report/expense-table-data';
+import { UserReportData } from 'src/app/models/report/user-report-data';
 import { User } from 'src/app/models/user.model';
 import { ReportService } from 'src/app/services/report.service';
 import { selectCurrentProject, selectUser } from 'src/app/state/app.selectors';
@@ -18,10 +20,14 @@ import { AppState } from 'src/app/state/app.state';
 export class UserReportComponent implements OnInit {
 
   currentProject$: Observable<Project>;
+  currentProject: Project;
   currentUser$: Observable<User>;
   userId: string;
 
+
   expenseData: ExpenseTableData[];
+  userReportData: UserReportData;
+  fairShares: { [key: string]: number } = {};
 
   constructor(
     private route: ActivatedRoute,
@@ -31,6 +37,7 @@ export class UserReportComponent implements OnInit {
 
   ngOnInit(): void {
     this.currentProject$ = this.store.select(selectCurrentProject);
+    this.currentProject$.subscribe(p => this.currentProject = p);
 
     this.currentUser$ = this.route.params.pipe(
       mergeMap((params) => {
@@ -40,6 +47,8 @@ export class UserReportComponent implements OnInit {
 
     combineLatest([this.currentProject$, this.currentUser$]).subscribe(([project, user]) => {
 
+      this.fairShares = project.getFairShares();
+      this.userReportData = this.reportService.getUserReportData(project, user);
       this.expenseData = this.reportService.getExpenseReportData(project, user);
 
     });
@@ -57,6 +66,48 @@ export class UserReportComponent implements OnInit {
 
   getDiffString(eData: ExpenseTableData) {
     return eData.diff > 0 ? `+${eData.diff.toFixed(2)}` : eData.diff.toFixed(2);
+  }
+
+  getTotalExpenses(expenses: Expense[]) {
+    return expenses.reduce((p, c) => p + c.value, 0);
+  }
+
+  getExpenseCountString(project: Project) {
+    let percent = this.userReportData.expenses.length * 100.0 / project.expenses.length;
+    return this.userReportData.expenses.length + '/' + project.expenses.length + ' (' + percent.toFixed(0) + '%)';
+  }
+
+  getExpenseValueString(project: Project) {
+    let percent = this.userReportData.expenses.length * 100.0 / project.expenses.length;
+    return "R$ " + this.userReportData.totalExpenses.toFixed(0) + '/' + this.getTotalExpenses(project.expenses).toFixed(0) + ` (${percent.toFixed(0)}%)`;
+  }
+
+  getExpensesPaid(user: User) {
+    let paid = this.userReportData.expenses.reduce((p, c) => {
+      return p + c.payers.reduce((p, c) => {
+        let paid = c.payer.id == user.id ? c.amount : 0;
+        return paid;
+      }, 0)
+    }, 0);
+    return paid;
+  }
+
+  getFairShare(user: User) {
+    return this.fairShares[user.id];
+  }
+
+  getBalance(user: User) {
+    let value: number = this.currentProject.getBalances()[user.id];
+    let sign = value > 0 ? "+" : "";
+    return "R$ " + sign + value.toFixed(2);
+  }
+
+  getPaymentsMade(user: User) {
+    return this.currentProject.getPaymentsMade(user);
+  }
+
+  getPaymentsReceived(user: User) {
+    return this.currentProject.getPaymentsReceived(user);
   }
 
 }
